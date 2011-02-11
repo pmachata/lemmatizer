98a832673369909fe77480f31e0d4cae97d5c118
Petr Machata <pmachata@gmail.com>
2011-02-11 05:24:20 +0100

    Extract key-formatting code for adjectives to externally visible method

--- adjective.cc
+++ adjective.cc
@@ -8,6 +8,53 @@ adjective_handler::adjective_handler ()
   : pos_handler ("adjective")
 {}
 
+std::string
+adjective_handler::format_form_key (gram_code_t degree, gram_code_t gcase,
+				    gram_code_t number, gram_code_t gender,
+				    bool animate, bool inanimate,
+				    bool secondary)
+{
+  if (degree == gm__invalid)
+    degree = gm_positive;
+  bool is_short = gcase == gm__invalid && degree == gm_positive;
+
+  std::ostringstream os;
+  os << format_rus (degree);
+
+  if (degree == gm_comparative)
+    {
+      if (secondary)
+	os << '.' << format_rus (gm_secondary);
+      else
+	os << ".primary";
+    }
+
+  if (is_short)
+    os << ".short";
+
+  if (number != gm__invalid)
+    {
+      if (gender != gm__invalid)
+	{
+	  assert (number == gm_singular);
+	  os << '.' << format_rus (gender);
+	}
+      else
+	{
+	  assert (number == gm_plural);
+	  os << '.' << format_rus (number);
+	}
+    }
+
+  if (gcase != gm__invalid)
+    os << '.' << format_rus (gcase);
+
+  if ((animate || inanimate) && !(animate && inanimate))
+    os << '.' << format_rus (animate ? gm_animate : gm_inanimate);
+
+  return os.str ();
+}
+
 void
 adjective_handler::fill_hdf (CAgramtab *agramtab,
 			     lemmatize::const_iterator const &it,
@@ -54,44 +101,8 @@ adjective_handler::fill_hdf (CAgramtab *agramtab,
 	throw std::runtime_error
 	  ("Don't know what 'secondary' means without degree.");
 
-      if (degree == gm__invalid)
-	degree = gm_positive;
-      bool is_short = gcase == gm__invalid && degree == gm_positive;
-
-      std::ostringstream os;
-      os << format_rus (degree);
-
-      if (degree == gm_comparative)
-	{
-	  if (secondary)
-	    os << '.' << format_rus (gm_secondary);
-	  else
-	    os << ".primary";
-	}
-
-      if (is_short)
-	os << ".short";
-
-      if (gnumber != gm__invalid)
-	{
-	  if (gender != gm__invalid)
-	    {
-	      assert (gnumber == gm_singular);
-	      os << '.' << format_rus (gender);
-	    }
-	  else
-	    {
-	      assert (gnumber == gm_plural);
-	      os << '.' << format_rus (gnumber);
-	    }
-	}
-
-      if (gcase != gm__invalid)
-	os << '.' << format_rus (gcase);
-
-      if ((animate || inanimate) && !(animate && inanimate))
-	os << '.' << format_rus (animate ? gm_animate : gm_inanimate);
-
-      data[os.str ()].push_back (std::make_pair (*ft, ft.accent ()));
+      std::string key = format_form_key (degree, gcase, gnumber, gender,
+					 animate, inanimate, secondary);
+      data[key].push_back (std::make_pair (*ft, ft.accent ()));
     }
 }
--- adjective.hh
+++ adjective.hh
@@ -12,6 +12,11 @@ public:
   virtual void fill_hdf (CAgramtab *agramtab,
 			 lemmatize::const_iterator const &it,
 			 hdf_data_map &data) const;
+
+  static std::string format_form_key (gram_code_t degree, gram_code_t gcase,
+				      gram_code_t number, gram_code_t gender,
+				      bool animate, bool inanimate,
+				      bool secondary);
 };
 
 class short_adjective_handler
