6cae1b0b2f7bc7870bcf0e765505d0a025e7b25c
Petr Machata <pmachata@gmail.com>
2011-02-11 06:22:19 +0100

    Add licensing boilerplate

--- main.cc
+++ main.cc
@@ -1,3 +1,19 @@
+// Copyright (C) 2011 Petr Machata
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+
 #include <cstring>
 #include <iconv.h>
 #include <iostream>
@@ -157,6 +173,7 @@ public:
 	 it != lem.end (); ++it)
       {
 	part_of_speech pos = it.get_part_of_speech ();
+	std::cerr << "Part of speech = " << show (pos.name ()) << std::endl;
 	lemmatize::forms forms = it.forms ();
 
 	pos_handler const *handler
